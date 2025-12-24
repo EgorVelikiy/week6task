1ac91a3482b32642f7e9f064db7fe34e6424097d
Egor <egor.velikiy.02@mail.ru>
2025-12-24 22:39:38 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,102 @@
+import * as crypto from "crypto"
+
+const CORS_HEADERS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT",
+    "Access-Control-Allow-Headers": "*",
+};
+
+const TEXT_PLAIN_HEADER = {
+    "Content-Type": "text/plain; charset=utf-8",
+};
+
+const SYSTEM_LOGIN = "20cf5726-c4a6-4653-b049-6e72b934e3d4";
+
+function corsMiddleware(req, res, next) {
+    res.set(CORS_HEADERS);
+    if (req.method === "OPTIONS") return res.sendStatus(204);
+    next();
+}
+
+function readFileAsync(filePath, createReadStream) {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        const stream = createReadStream(filePath);
+
+        stream.on("data", (chunk) => chunks.push(chunk));
+        stream.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+        stream.on("error", (err) => reject(err));
+    });
+}
+
+function generateSha1Hash(text) {
+    return crypto.createHash("sha1").update(text).digest("hex");
+}
+
+function readHttpResponse(response) {
+    return new Promise((resolve, reject) => {
+        const chunks = [];
+        response.on("data", (chunk) => chunks.push(chunk));
+        response.on("end", () => resolve(Buffer.concat(chunks).toString("utf8")));
+        response.on("error", (err) => reject(err));
+    });
+}
+
+async function fetchUrlData(url) {
+    return new Promise((resolve, reject) => {
+        http.get(url, async (response) => {
+            try {
+                const data = await readHttpResponse(response);
+                resolve(data);
+            } catch (err) {
+                reject(err);
+            }
+        }).on("error", reject);
+    });
+}
+
+export default function createApp(express, bodyParser, createReadStream, currentFilePath) {
+    const app = express();
+
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
+    app.use(corsMiddleware);
+
+    app.get("/login/", (_req, res) => {
+        res.set(TEXT_PLAIN_HEADER).send(SYSTEM_LOGIN);
+    });
+
+    app.get("/code/", async (_req, res) => {
+        const fileContent = await readFileAsync(currentFilePath, createReadStream);
+        res.set(TEXT_PLAIN_HEADER).send(fileContent);
+    });
+
+    app.get("/sha1/:input/", (req, res) => {
+        const hash = generateSha1Hash(req.params.input);
+        res.set(TEXT_PLAIN_HEADER).send(hash);
+    });
+
+    app.get("/req/", async (req, res) => {
+        try {
+            const data = await fetchUrlData(req.query.addr);
+            res.set(TEXT_PLAIN_HEADER).send(data);
+        } catch (err) {
+            res.status(500).send(err.toString());
+        }
+    });
+
+    app.post("/req/", async (req, res) => {
+        try {
+            const data = await fetchUrlData(req.body.addr);
+            res.set(TEXT_PLAIN_HEADER).send(data);
+        } catch (err) {
+            res.status(500).send(err.toString());
+        }
+    });
+
+    app.all(/.*/, (_req, res) => {
+        res.set(TEXT_PLAIN_HEADER).send(SYSTEM_LOGIN);
+    });
+
+    return app;
+}
